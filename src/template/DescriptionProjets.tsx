2e3da543be1a9c73442cae8274edc0ce707f3536
SimonR99 <simonroy99@hotmail.ca>
2023-03-14 22:04:36 -0400

    add content to DescriptionProjets

--- src/template/DescriptionProjets.tsx
+++ src/template/DescriptionProjets.tsx
@@ -3,13 +3,12 @@ import { Section } from '../layout/Section';
 
 const DescriptionProjets = () => (
   <Section
-    title="Club QuantumETS"
-    subtitle="Activités"
-    description="Voici une liste des activités que QuantumETS participe pour découvrir et approfondir les connaissances de ses membres sur les ordinateurs quantiques."
+    title="Projets"
+    description="Le club offre la possibilité aux étudiants de participer à des projets pour développer leurs compétences et leurs connaissances dans le domaine de l&rsquo;informatique quantique."
   >
-    <div className="grid grid-cols-1 gap-x-4 gap-y-16 sm:grid-cols-2 md:grid-cols-3">
+    <div className="grid grid-cols-1 gap-x-4 gap-y-16 sm:grid-cols-2 md:grid-cols-4">
       <FeatureElement
-        title="Hack-a-thon"
+        title="Soutien"
         icon={
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -23,11 +22,11 @@ const DescriptionProjets = () => (
           </svg>
         }
       >
-        Nous participons à une panoplie d&rsquo;évènements de style Hack-a-thon
-        comme le QHack ou le QcHack.
+        Soutien aux étudiants désirants faire un projet dans le domaine de
+        l&rsquo;informatique quantique avec l&rsquo;équipe du club.
       </FeatureElement>
       <FeatureElement
-        title="Technologies"
+        title="Partenaire"
         icon={
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -37,16 +36,14 @@ const DescriptionProjets = () => (
             strokeLinejoin="round"
           >
             <path d="M0 0h24v24H0z" stroke="none" />
-            <path d="M12 12v.01M19.071 4.929a4 10 45 00-9.9 4.243 4 10 45 00-4.242 9.9 4 10 45 009.9-4.244 4 10 45 004.242-9.9" />
-            <path d="M4.929 4.929a10 4 45 004.243 9.9 10 4 45 009.9 4.242 10 4 45 00-4.244-9.9 10 4 45 00-9.9-4.242" />
+            <path d="M21 12h-4l-3 8-4-16-3 8H3" />
           </svg>
         }
       >
-        Apprentissage des technologies de pointe pour le développement de
-        système quantique (Pennylane, Qiskit, IBM Composer, etc.)
+        Résolution de problèmes concrêts avec des partenaires externes.
       </FeatureElement>
       <FeatureElement
-        title="Connaissances"
+        title="Variété d'outils"
         icon={
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -56,13 +53,30 @@ const DescriptionProjets = () => (
             strokeLinejoin="round"
           >
             <path d="M0 0h24v24H0z" stroke="none" />
-            <path d="M3 17l6-6 4 4 8-8" />
-            <path d="M14 7h7v7" />
+            <path d="M21 12h-4l-3 8-4-16-3 8H3" />
           </svg>
         }
       >
-        Un environnement ouvert pour partager ses connaissances et en apprendre
-        plus sur la programmation quantique.
+        Utilisation de technologies variées comme Qiskit, Cirq, Q#, Pennylane,
+        braket, etc.
+      </FeatureElement>
+      <FeatureElement
+        title="Reproduction d&rsquo;articles scientifiques"
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M0 0h24v24H0z" stroke="none" />
+            <path d="M21 12h-4l-3 8-4-16-3 8H3" />
+          </svg>
+        }
+      >
+        Le clubs offre la possibilité aux étudiants de reproduire des articles
+        scientifiques de difficulté variable.
       </FeatureElement>
     </div>
   </Section>
