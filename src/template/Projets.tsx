dbc785294c153cea97f3c6838ca8821c94455d14
Kuift Kotar <kuiftk@gmail.com>
2023-03-16 07:45:08 -0400

    changed synth project description

--- src/template/Projets.tsx
+++ src/template/Projets.tsx
@@ -15,11 +15,38 @@ const Projets = () => (
         reverse={false}
       >
         <p>
-          Le synthétiseur est un projet qui a pour but de créer un plugin pour
-          le logiciel de synthèse musicale Ableton Live. Ce plugin permettra de
-          créer des sons à l&rsquo;aide d&rsquo;un ordinateur quantique. Il est
-          actuellement en développement.
+          Nous sommes présentement dans le processus de développement d’un
+          plug-in audio qui fonctionne avec des DAWs comme Reaper et Ableton
+          Live. Ce plug-in sera capable d’appliquer des effets audio, créer des
+          tables d’ondes ainsi que synthétiser du son directement à partir d’un
+          ordinateur quantique. Les buts de ce projet sont :
         </p>
+        <ul className="list-disc">
+          <li>
+            en apprendre davantage sur le traitement de signaux avec les
+            ordinateurs quantiques
+          </li>
+          <li>
+            créer une application utilisable par tout le monde qui utilise un
+            ordinateur quantique
+          </li>
+          <li>
+            consolider plusieurs papiers de recherche sous un même projet en
+            créant des opérateurs pour la synthèse sonore soustractive
+          </li>
+          <li>
+            potentiellement découvrir une méthode alternative de synthèse sonore
+            qui pourrait être utile pour créer certains types de son
+            difficilement faisable par synthèse par table d’ondes
+          </li>
+          <li>
+            exploiter les propriétés particulières aux ordinateurs quantiques et
+            explorer l’avantage quantique dans ce domaine
+          </li>
+          <li>
+            ouvrir la porte à un projet pour faire un haut-parleur quantique??
+          </li>
+        </ul>
       </ProjetRow>
     </Section>
   </Background>
