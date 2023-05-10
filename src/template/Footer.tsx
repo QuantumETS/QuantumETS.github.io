5feb202b0c9af4a61c2840dbe623eaa31430f963
SimonR99 <simonroy99@hotmail.ca>
2023-05-10 15:02:54 -0400

    add notebooks

--- src/template/Footer.tsx
+++ src/template/Footer.tsx
@@ -52,6 +52,32 @@ const Footer = () => (
               </Link>
             </li>
           </FooterLinks>
+          <FooterLinks title="Notebook">
+            <li>
+              <Link
+                target="_blank"
+                href="https://deepnote.com/workspace/quantum-ets-bd6b11e4-0d15-4576-90e5-88412c5f674e/project/2-Qubit-et-superposition-a3b45c64-9522-4af8-a9fa-82004b07f9df/notebook/Notebook%201-e33f80205bf34ca381bea01f5f45a816"
+              >
+                Informatique quantique - Qubit et superposition
+              </Link>
+            </li>
+            <li>
+              <Link
+                target="_blank"
+                href="https://deepnote.com/workspace/quantum-ets-bd6b11e4-0d15-4576-90e5-88412c5f674e/project/1-Introduction-a-Pennylane-0c99f634-1e28-4587-813c-5f8dac4d40e7/"
+              >
+                Pennylane - Introduction
+              </Link>
+            </li>
+            <li>
+              <Link
+                target="_blank"
+                href="https://deepnote.com/workspace/quantum-ets-bd6b11e4-0d15-4576-90e5-88412c5f674e/project/2-Portes-quantiques-2b505ac8-bbd4-4fed-8a32-48eafb49d53a/notebook/2.%20Portes%20quantiques-feb2121453e441f3a601a3cbbef890c2"
+              >
+                Pennylane - Portes quantiques
+              </Link>
+            </li>
+          </FooterLinks>
         </div>
       </FooterTwoRowsCopyright>
     </Section>
