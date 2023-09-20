8e0891db5cdb630aa6d4a49b741962aceb9ad3e7
SimonR99 <simonroy99@hotmail.ca>
2023-09-20 16:39:29 -0400

    fix : url typo and remove file

--- src/template/Pricing.tsx
+++ src/template/Pricing.tsx
@@ -6,7 +6,7 @@ const Pricing = () => (
     <div className="grid grid-cols-1 gap-3 md:grid-cols-1">
       <div className="m-20 flex flex-col items-center justify-center">
         <a
-          href="http://localhost:3000/assets/pdf/Quantum-plan-partenariat_v1.pdf"
+          href="https://quantumets.etsmtl.ca/assets/pdf/Quantum-plan-partenariat_v1.pdf"
           className="mt-8 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
         >
           Plan de partenariat
