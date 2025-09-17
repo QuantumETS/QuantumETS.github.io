ec7f2c798e84bee4b2154fe068329a036a2002b7
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2025-09-17 14:05:22 -0400

    directing href to actual event

--- src/template/EventBanner.tsx
+++ src/template/EventBanner.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const EventBanner = () => (
   <Link
-    href="https://quantuemets-events.com"
+    href="/events/QFF25_FR"
     className="block bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500 py-3 text-center text-xl font-bold text-white transition-opacity hover:opacity-80"
   >
     Inscrivez-vous! - Qiskit Fall Fest 2025 - Register Now!
