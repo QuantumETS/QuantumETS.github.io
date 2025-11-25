cf99b671a62a28436f63bacdc8cd872ed47a6f7c
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2025-11-25 13:19:51 -0500

    mise à jour du lien vers plan de commandite

--- src/template/Pricing.tsx
+++ src/template/Pricing.tsx
@@ -66,8 +66,10 @@ const Pricing = () => (
         </div>
         <div className="m-5 flex flex-col items-center justify-center">
           <a
-            href="https://quantumets.etsmtl.ca/assets/pdf/Quantum-plan-partenariat_v1.pdf"
+            href="https://etsmtl365-my.sharepoint.com/:b:/g/personal/quantumets_etsmtl_ca/IQDNeYCW8S45TYlEZYwlbAwFAVXJR4cDzZ17ht6NJReI2PY?e=tgMZQS"
             className="mt-8 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Plan de partenariat
           </a>
