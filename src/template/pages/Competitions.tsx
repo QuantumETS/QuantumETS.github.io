000965e78b6b981848a92612b631472c7eae5e71
Raed Alkhatib <m.raed.khatib@gmail.com>
2026-05-25 20:41:12 -0400

    feature: ajouter bouton pour le vlog YQuantum

--- src/template/pages/Competitions.tsx
+++ src/template/pages/Competitions.tsx
@@ -1,3 +1,4 @@
+import { ActionButtonSection } from '../../cta/ActionButtonSection';
 import { Meta } from '../../layout/Meta';
 import { AppConfig } from '../../utils/AppConfig';
 import { DescriptionCompetitions } from '../DescriptionCompetitions';
@@ -11,6 +12,17 @@ const Competitions = () => (
     <Navbar selectedTitle="Compétitions" />
     <DescriptionCompetitions />
     <Events />
+    <ActionButtonSection
+      body={
+        <p>
+          Découvrez l&rsquo;expérience du club en compétition à travers notre
+          vlog tourné lors du hackathon YQuantum 2025 à l&rsquo;Université Yale.
+        </p>
+      }
+      buttonLabel="VLOG YQuantum"
+      actionUrl="https://youtu.be/MmPx3GXDZ3A"
+      icon="external"
+    />
     <Footer />
   </div>
 );
