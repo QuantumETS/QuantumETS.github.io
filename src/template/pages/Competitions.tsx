b91abcaeedf85919c9de9ed127bc80ecf0ec0a27
Raed Alkhatib <m.raed.khatib@gmail.com>
2026-05-25 20:34:43 -0400

    feature: deplacer le tableau des compétitions de l'acceuil vers la nouvelle page

--- src/template/pages/Competitions.tsx
+++ src/template/pages/Competitions.tsx
@@ -1,6 +1,7 @@
 import { Meta } from '../../layout/Meta';
 import { AppConfig } from '../../utils/AppConfig';
 import { DescriptionCompetitions } from '../DescriptionCompetitions';
+import { Events } from '../Events';
 import { Footer } from '../Footer';
 import { Navbar } from '../Navbar';
 
@@ -9,6 +10,7 @@ const Competitions = () => (
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Navbar selectedTitle="Compétitions" />
     <DescriptionCompetitions />
+    <Events />
     <Footer />
   </div>
 );
